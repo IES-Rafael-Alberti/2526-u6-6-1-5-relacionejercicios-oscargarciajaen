d29914415dbbd16209efeb222c93ca4945645a12
oscargarcia <ogarjae565@g.educaand.es>
2026-03-02 13:14:09 +0100

    Modificando los println y añadiendo la teoria al md

--- src/main/kotlin/es/ies/ejercicios/u6/ej61/Especificacion/MotorCobustion.kt
+++ src/main/kotlin/es/ies/ejercicios/u6/ej61/Especificacion/MotorCobustion.kt
@@ -6,12 +6,10 @@ class MotorCobustion(numeroBastidores: Int): Motor(numeroBastidores) {
     var combustible = 20
 
     override fun arrancar() {
-        println("Introduce la llave, la gira y el motor arranca")
         arrancado = true
     }
 
     override fun detener() {
-        println("Gira la llave de nuevo y el motor se detiene")
         arrancado = false
     }
 
@@ -20,35 +18,28 @@ class MotorCobustion(numeroBastidores: Int): Motor(numeroBastidores) {
     }
 
     override fun acelerar() {
-        println("El motor acelera")
         revoluciones += 20
     }
 
     override fun frenar() {
         if (revoluciones == 0){
-            println("El motor ya esta frenado al máximo")
         } else {
             revoluciones -= 20
-            println("El motor frena")
         }
     }
 
     override fun obtenerRevoluciones(): Int {
-        println("Revoluciones ${revoluciones}")
         return revoluciones
     }
 
     override fun obtenerEnergia(): Int {
-        println("Tiene ${combustible} litros")
         return combustible
     }
 
     override fun aumentarEnergia() {
         if (combustible == 100){
-            println("Energía al máximo")
         } else {
             combustible += 20
-            println("Echando combustible")
         }
     }
 }
--- src/main/kotlin/es/ies/ejercicios/u6/ej61/Especificacion/MotorElectrico.kt
+++ src/main/kotlin/es/ies/ejercicios/u6/ej61/Especificacion/MotorElectrico.kt
@@ -6,12 +6,10 @@ class MotorElectrico(numeroBastidores: Int): Motor(numeroBastidores) {
     var porcentajeBateria = 20
 
     override fun arrancar() {
-        println("Pulsa el botón de encender y el motor se enciende")
         encendido = true
     }
 
     override fun detener() {
-        println("Pulsa el botón de apagar y el motor se apaga")
         encendido = false
     }
 
@@ -21,35 +19,28 @@ class MotorElectrico(numeroBastidores: Int): Motor(numeroBastidores) {
 
     override fun acelerar() {
         revoluciones += 20
-        println("El motor acelera")
     }
 
     override fun frenar() {
         if (revoluciones == 0) {
-            println("El motor ya esta frenado al máximo")
         } else {
             revoluciones -= 20
-            println("El motor frena")
         }
     }
 
     override fun obtenerRevoluciones(): Int {
-        println("Revoluciones ${revoluciones}")
         return revoluciones
     }
 
     override fun obtenerEnergia(): Int{
-        println("Bateria al ${porcentajeBateria}% ")
         return porcentajeBateria
 
     }
 
     override fun aumentarEnergia() {
         if (porcentajeBateria == 100){
-            println("La batería está al máximo")
         } else {
             porcentajeBateria += 20
-            println("El motor está cargando")
         }
     }
 }
